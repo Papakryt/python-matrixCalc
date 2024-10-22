cee06cf955d52e724cb9a8f156dd5c3cc6a23d59
YukiNo <kryt1990@bk.ru>
2024-10-23 00:18:50 +0300

    PK2

--- matrixCalc/matrixCalc.py
+++ matrixCalc/matrixCalc.py
@@ -13,7 +13,6 @@ class Matrix:
             for i in range(0,self.m):
                 Mem=[0]*(n)
                 self.M.append(Mem)
-
     def writeMatrix(self):
         for i in self.M:
             stroke = ""
@@ -52,28 +51,19 @@ class Matrix:
                         mem+=int(self.indexElement(i,c))*int(M.indexElement(c,j))
                     res.indexFill(i,j,str(mem))
             return res
-
-        
-
-
+    def transMatrix(self):
+        res=Matrix(self.n,self.m,0)
+        for i in range(0,self.m):
+            for j in range(0,self.n):
+                res.indexFill(j,i,self.M[i][j])
+        return res
+    
 m1=int(input("Wrrite line first matrix: "))
 n1=int(input("Wrrite column first matrix: "))
-m2=int(input("Wrrite line second matrix: "))
-n2=int(input("Wrrite column second matrix: "))
 print("Info first matrix")
 M1=Matrix(m1,n1,1)
-print("Info second matrix")
-M2=Matrix(m2,n2,1)
 print("First matrix")
 M1.writeMatrix()
-print("Second matrix")
+M2=M1.transMatrix()
+print("Trans")
 M2.writeMatrix()
-M3=M1.sumMatrix(M2)
-print("Sum")
-M3.writeMatrix()
-M4=M2.subMatrix(M1)
-print("Sub")
-M4.writeMatrix()
-M5=M1.multiMatrix(M2)
-print("Multi")
-M5.writeMatrix()
